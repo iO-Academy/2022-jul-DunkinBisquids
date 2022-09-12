3d62a4b296b015af4b01fb7230cb129ad64e527b
aidanmaskell <aidan.maskell@gmail.com>
2022-09-12 15:42:41 +0100

    updated styling on biscuit card

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ require_once './vendor/autoload.php';
 <!DOCTYPE html>
 <html lang="en-gb">
 <head>
-	<title>Transformers</title>
+	<title>Bisquids</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link href="style.css" type="text/css" rel="stylesheet" />
+    <link href="styles.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body class="container">
