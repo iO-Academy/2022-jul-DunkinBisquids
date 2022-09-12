a04a464261943a47420494fd127ec7b50c8eca6c
aidanmaskell <aidan.maskell@gmail.com>
2022-09-12 15:10:45 +0100

    added viewhelper to dsiplay biscuits, left in hard coded test function to be deleted on review

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
+use BisquidsTin\ViewHelpers\BiscuitsViewHelper;
+
 require_once './vendor/autoload.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+	<title>Transformers</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="style.css" type="text/css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body class="container">
+    <?php echo BiscuitsViewHelper::biscTest() ?>
+</body>
+</html>
