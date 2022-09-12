41d04ab265883ce50a1e3fd95fc950ff258f08f2
erc-clarkson <erc.clarkson@btinternet.com>
2022-09-12 16:12:24 +0100

    add nav bar and background image to index file

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
 require_once './vendor/autoload.php';
+
+?>
+<html>
+    <head>
+        <title>Dunkin' Bisquids</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <nav class="container-fluid bg-white ">
+            <div class="row">
+                <h1 class="text-center title p-lg-5 p-2">Dunkin' Bisquids</h1>
+            </div>
+        </nav>
+        <main>
+            <img class="logoImg" src="./Dunkin_Donut_Logo.png" alt="Dunkin_Bisquids_Logo">
+            <section>
+                
+            </section>
+        </main>
+    </body>
+</html>
