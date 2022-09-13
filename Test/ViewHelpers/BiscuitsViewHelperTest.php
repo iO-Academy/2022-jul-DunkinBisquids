d8d5f43fd782e844c3ea33ae840cedd48a3c15ac
mikeycodingstuff <mikeyinguk@gmail.com>
2022-09-13 11:45:23 +0100

    changed padding on biscuit titles

--- Test/ViewHelpers/BiscuitsViewHelperTest.php
+++ Test/ViewHelpers/BiscuitsViewHelperTest.php
@@ -18,7 +18,7 @@ class BiscuitsViewHelperTest extends Testcase
 
         $expected = '<div class="card d-flex flex-direction-column align-items-center p-3 m-4 col-10 col-lg-3">';
         $expected .= '<div class="card-title rounded">';
-        $expected .= '<h2 class="text-center py-2">Digestive</h2>';
+        $expected .= '<h2 class="text-center p-2">Digestive</h2>';
         $expected .= '</div><div class="card-img d-flex justify-content-center mb-3">';
         $expected .= '<img src="https://s3-us-west-1.amazonaws.com/contentlab.studiod/getty/d05662cfb32042c9894dddf8ed73ce22.jpg" class="rounded mw-100" alt="Digestive" />';
         $expected .= '</div><form action="biscuitDetails.php" method="POST">';
