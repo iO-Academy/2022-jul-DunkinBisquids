82da2c40c1e496f663d019357d4154f26fe89283
mikeycodingstuff <mikeyinguk@gmail.com>
2022-09-13 11:35:20 +0100

    updated unit test for viewhelper

--- Test/ViewHelpers/BiscuitsViewHelperTest.php
+++ Test/ViewHelpers/BiscuitsViewHelperTest.php
@@ -12,15 +12,19 @@ class BiscuitsViewHelperTest extends Testcase
         $biscuitMock = $this->createMock(Biscuits::class);
         $biscuitMock->method('getName')->willReturn('Digestive');
         $biscuitMock->method('getImg')->willReturn('https://s3-us-west-1.amazonaws.com/contentlab.studiod/getty/d05662cfb32042c9894dddf8ed73ce22.jpg');
+        $biscuitMock->method('getId')->willReturn(1);
         
         $input = [$biscuitMock];
 
         $expected = '<div class="card d-flex flex-direction-column align-items-center p-3 m-4 col-10 col-lg-3">';
         $expected .= '<div class="card-title rounded">';
         $expected .= '<h2 class="text-center py-2">Digestive</h2>';
-        $expected .= '</div><div class="card-img d-flex justify-content-center">';
+        $expected .= '</div><div class="card-img d-flex justify-content-center mb-3">';
         $expected .= '<img src="https://s3-us-west-1.amazonaws.com/contentlab.studiod/getty/d05662cfb32042c9894dddf8ed73ce22.jpg" class="rounded mw-100" alt="Digestive" />';
-        $expected .= '</div></div>';
+        $expected .= '</div><form action="biscuitDetails.php" method="POST">';
+        $expected .= '<input type="hidden" value="1" />';
+        $expected .= '<button type="button" class="btn btn-secondary">More Info</button>';
+        $expected .= '</form></div>';
 
         $actual = BiscuitsViewHelper::displayAllBiscuits($input);
         $this->assertEquals($expected, $actual);
