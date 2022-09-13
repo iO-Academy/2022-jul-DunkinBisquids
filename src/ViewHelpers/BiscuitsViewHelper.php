5934fe78adf19e9bcd6c2e0cfaa49d57b5fb40cf
mikeycodingstuff <mikeyinguk@gmail.com>
2022-09-13 11:33:30 +0100

    added styling to button via bootstrap

--- src/ViewHelpers/BiscuitsViewHelper.php
+++ src/ViewHelpers/BiscuitsViewHelper.php
@@ -21,11 +21,11 @@ class BiscuitsViewHelper
             $result .= '<div class="card d-flex flex-direction-column align-items-center p-3 m-4 col-10 col-lg-3">';
             $result .= '<div class="card-title rounded">';
             $result .= '<h2 class="text-center py-2">' . $biscuit->getName() . '</h2>';
-            $result .= '</div><div class="card-img d-flex justify-content-center">';
+            $result .= '</div><div class="card-img d-flex justify-content-center mb-3">';
             $result .= '<img src="' . $biscuit->getImg() . '" class="rounded mw-100" alt="' . $biscuit->getName() . '" />';
             $result .= '</div><form action="biscuitDetails.php" method="POST">';
             $result .= '<input type="hidden" value="' . $biscuit->getId() . '" />';
-            $result .= '<button>More info</button>';
+            $result .= '<button type="button" class="btn btn-secondary">More Info</button>';
             $result .= '</form></div>';
         }
         return $result;
