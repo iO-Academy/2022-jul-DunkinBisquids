13c727ff2d3cb3291fd4ed262feeec28f52d2465
aidanmaskell <aidan.maskell@gmail.com>
2022-09-13 13:53:39 +0100

    added db connection

--- hiddenVoting.php
+++ hiddenVoting.php
@@ -2,8 +2,10 @@
 
 session_start();
 
+use BisquidsTin\Hydrators\BiscuitsHydrator;
 use BisquidsTin\Utilities\DB;
 
 require_once './vendor/autoload.php';
 
 $db = DB::getDB();
+$biscuits = BiscuitsHydrator::getBiscuits($db);
