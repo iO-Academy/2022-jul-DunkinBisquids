7f362a5d8fc9774bfa8192474b91cb9c3e2df0d3
aidanmaskell <aidan.maskell@gmail.com>
2022-09-14 11:35:11 +0100

    added new class to handle biscuit data processing

---
+++ src/Utilities/BiscuitDataProcessor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BisquidsTin\Utilities;
+
+use BisquidsTin\Classes\Biscuit;
+
+// abstract class BiscuitDataProcessor 
+// {
+//     public static function mostDunked(array $biscuits):  string
+//     {
+//         usort($biscuits, function($first, $second)
+//         {
+//             return $first->getDunk()
+//         }
+
+//         usort($allTransformers, function($first, $second)
+//         {
+//                 return $first->getWinRatio() < $second->getWinRatio();
+//         });
+
+//         $topFive = array_slice($allTransformers, 0, 5);
+
+//         return $topFive;
+//     }
+//     }
+// }
